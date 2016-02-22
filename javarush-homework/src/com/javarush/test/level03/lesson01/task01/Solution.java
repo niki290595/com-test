37fc895beb6c90f39741912a9110fc83914b6b03
ilya <nikitin_ilya@outlook.com>
2016-02-23 01:14:03 +0500

    add tasks from level03

---
+++ javarush-homework/src/com/javarush/test/level03/lesson01/task01/Solution.java
@@ -0,0 +1,13 @@
+﻿package com.javarush.test.level03.lesson01.task01;
+
+/* Реализуйте метод public static void div(int a, int b). 
+Метод должен делить первое число на второе, и выводить на экран результат деления а / b.
+*/
+
+public class Solution
+{
+	public static void div(int a, int b)
+	{
+		System.out.println(a/b);
+	}
+}
