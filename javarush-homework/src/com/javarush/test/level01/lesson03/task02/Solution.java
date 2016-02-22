85a6c1b84368a93b2eddc25719342c8f3ea50815
ilya <nikitin_ilya@outlook.com>
2016-02-23 00:22:33 +0500

    add tasks from level01

---
+++ javarush-homework/src/com/javarush/test/level01/lesson03/task02/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level01.lesson03.task02;
+
+/* ‚ывод на экран переменной
+ЋбъЯвите переменную name типа String.
+‘разу же в строке объЯвлениЯ присвойте ей какое-нибудь значение.
+‚ыведите на экран переменную name.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String name = "JavaRush";
+        System.out.println(name);
+    }
+}
