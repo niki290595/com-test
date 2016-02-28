387644774499f20165d71dbc0ff991776aacb4e5
ilya <nikitin_ilya@outlook.com>
2016-02-28 22:15:27 +0500

    modify solution

--- psu/com-test/CarCOM/CarClientInCSharp/Program.cs
+++ psu/com-test/CarCOM/CarClientInCSharp/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace CarClientInCSharp
 {
+    [Guid("68B57DA1-999F-4768-8AA5-B15217F8E6EF")]
+    public class ExplicitGuid { }
+
+    public class NoExplicitGuid { }
+
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+            Type explicitType = typeof(ExplicitGuid);
+            Guid explicitGuid = explicitType.GUID;
+
+            Type explicitCOM = Type.GetTypeFromCLSID(explicitGuid);
+            Console.WriteLine("ExplicitType = {0};", explicitType.Name);
+            Console.WriteLine("ExplicitGuid = {0};", explicitGuid);
+            Console.WriteLine("ExplicitCOM = {0};", explicitCOM.Name);
+            Console.WriteLine("Created {0} type from CLSID {1};\n", explicitCOM.Name, explicitGuid);
+
+            Console.WriteLine("{0} and {1} equal: {2};\n",
+                        explicitType.Name, explicitCOM.Name,
+                        explicitType.Equals(explicitCOM));
+
+            // Instantiate an ExplicitGuid object.
+            try {
+                object obj = Activator.CreateInstance(explicitCOM);
+                Console.WriteLine("Instantiated {0} object = ", obj.GetType().Name);
+                Console.WriteLine("Instantiated {0} object. Full name = ", obj.GetType().FullName);
+
+                /*
+                Object[] argArray = new Object[1];
+                argArray[0] = 50;
+                explicitCOM.InvokeMember("SetMaxSpeed", System.Reflection.BindingFlags.InvokeMethod, null, obj, argArray);*/
+            } catch (COMException e) {
+                Console.WriteLine("COM Exception:\n{0}\n", e.Message);
+            }
         }
     }
 }
